ca157cc0169e3058ff688785bcbaacee6e3d4f2a
Ivan Kishchenko <darvik@crearts.xyz>
2023-12-03 13:15:46 +0700

    support source

--- event_bus.go
+++ event_bus.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+type Source interface {
+}
+
 type Handler interface {
 }
 
@@ -37,9 +40,11 @@ func WithPoolSize(poolSize int) Option {
 
 type EventBus interface {
 	Subscribe(h Handler) bool
-	Fire(e Event)
-	Send(e Event)
-	Schedule(d time.Duration, rep bool, e Event)
+	FireAnonymous(e Event)
+	Fire(s Source, e Event)
+	SendAnonymous(e Event)
+	Send(s Source, e Event)
+	Schedule(s Source, d time.Duration, rep bool, e Event)
 	Shutdown()
 }
 
@@ -55,13 +60,13 @@ func New(opts ...Option) EventBus {
 
 	bus := &eventBus{
 		mutex: sync.Mutex{},
-		bus:   make(chan Event, options.cacheSize),
+		bus:   make(chan busItem, options.cacheSize),
 	}
 
 	for idx := 0; idx < options.poolSize; idx++ {
 		go func() {
 			for e := range bus.bus {
-				bus.callHandlers(e)
+				bus.callHandlers(e.s, e.e)
 			}
 		}()
 	}
@@ -78,32 +83,48 @@ type eventHandlerMeta struct {
 	callback reflect.Value
 }
 
+type busItem struct {
+	s Source
+	e Event
+}
+
 type eventBus struct {
 	handlers []eventHandlerMeta
 	mutex    sync.Mutex
-	bus      chan Event
+	bus      chan busItem
 }
 
 func validate(arg, origin reflect.Type) bool {
 	return arg.Name() == origin.Name() && arg.PkgPath() == origin.PkgPath()
 }
 
-func (bus *eventBus) callHandlers(e Event) {
+func (bus *eventBus) callHandlers(s Source, e Event) {
 	bus.mutex.Lock()
 	copyHandlers := make([]eventHandlerMeta, len(bus.handlers))
 	copy(copyHandlers, bus.handlers)
 	bus.mutex.Unlock()
 	for _, h := range copyHandlers {
-		bus.tryCall(e, h)
+		bus.tryCall(s, e, h)
 	}
 }
 
-func (bus *eventBus) tryCall(e Event, meta eventHandlerMeta) bool {
-	if !validate(reflect.ValueOf(e).Type(), meta.callback.Type().In(1)) {
+func (bus *eventBus) tryCall(s Source, e Event, meta eventHandlerMeta) bool {
+	if s == nil {
+		s = bus
+	}
+	idx := meta.callback.Type().NumIn()
+	if idx < 2 {
+		return false
+	}
+	if !validate(reflect.ValueOf(e).Type(), meta.callback.Type().In(idx-1)) {
 		return false
 	}
 
-	meta.callback.Call([]reflect.Value{reflect.ValueOf(bus), reflect.ValueOf(e)})
+	if meta.callback.Type().NumIn() == 2 {
+		meta.callback.Call([]reflect.Value{reflect.ValueOf(bus), reflect.ValueOf(e)})
+	} else {
+		meta.callback.Call([]reflect.Value{reflect.ValueOf(bus), reflect.ValueOf(s), reflect.ValueOf(e)})
+	}
 
 	return true
 }
@@ -130,8 +151,9 @@ func (bus *eventBus) Subscribe(h Handler) bool {
 		meta.callback = method
 	}
 
-	// validate must be 2 arguments
-	if meta.callback.Type().NumIn() != 2 {
+	// validate must be 2,3 arguments
+	numIn := meta.callback.Type().NumIn()
+	if numIn < 2 || numIn > 3 {
 		return false
 	}
 
@@ -145,24 +167,32 @@ func (bus *eventBus) Subscribe(h Handler) bool {
 	return true
 }
 
-func (bus *eventBus) Fire(e Event) {
-	bus.bus <- e
+func (bus *eventBus) Fire(s Source, e Event) {
+	bus.bus <- busItem{s, e}
 }
 
-func (bus *eventBus) Send(e Event) {
-	bus.callHandlers(e)
+func (bus *eventBus) Send(s Source, e Event) {
+	bus.callHandlers(s, e)
 }
 
-func (bus *eventBus) Schedule(d time.Duration, rep bool, e Event) {
+func (bus *eventBus) FireAnonymous(e Event) {
+	bus.Fire(nil, e)
+}
+
+func (bus *eventBus) SendAnonymous(e Event) {
+	bus.Send(nil, e)
+}
+
+func (bus *eventBus) Schedule(s Source, d time.Duration, rep bool, e Event) {
 	go func() {
 		if rep {
 			ticker := time.NewTicker(d)
 			for range ticker.C {
-				bus.Fire(e)
+				bus.Fire(s, e)
 			}
 		} else {
 			time.AfterFunc(d, func() {
-				bus.Fire(e)
+				bus.Fire(s, e)
 			})
 		}
 	}()
